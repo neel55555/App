260a053dfadbad1da6a28de09b9887adea7c4b63
Neel Ion <neelion333@gmail.com>
2020-02-16 18:17:10 +0600

    1.1

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,51 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   build(BuildContext context) {
-
     return MaterialApp(
-
-      title: 'Hello World', 
+      title: 'Hello World',
       theme: ThemeData(
-        primaryColor: Colors.red[500], 
+        primaryColor: Colors.red[500],
       ),
       home: Scaffold(
         drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(child: Text('Drawer Header'), ), 
-              ListTile(title: Text('Hello World')), 
-              ListTile(title: Text('Hello World')), 
-              ListTile(title: Text('Hello World')), 
-              ListTile(title: Text('Hello World')), 
-              ListTile(title: Text('Hello World')), 
-            ],
-          )
-        ),
+            child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('Drawer Header'),
+            ),
+            ListTile(title: Text('Hello World')),
+            ListTile(title: Text('Hello World')),
+            ListTile(title: Text('Hello World')),
+            ListTile(title: Text('Hello World')),
+            ListTile(title: Text('Hello World')),
+          ],
+        )),
         appBar: AppBar(
           title: Text('Hello World'),
         ),
-        body: Text('Hello World'), 
-
+        body: Container(
+          height: 40,
+          width: 40,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Container(
+                width: 40.0,
+                height: 40.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage("https://i.imgur.com/BoN9kdC.png")
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-
     );
-
   }
-
 }
